d4e6067ebd5a7d975a8e69d4e03ac8065adb31c4
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-19 06:09:29 -0300

    Refatorando transformador model mapper

--- src/main/java/com/api/galaxy/config/Modelmapper.java
+++ src/main/java/com/api/galaxy/config/Modelmapper.java
@@ -1,13 +1,14 @@
 package com.api.galaxy.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Modelmapper {
-  
+
 	@Bean
-	public Modelmapper mapper() {
-		return new Modelmapper();
+	public ModelMapper mapper() {
+		return new ModelMapper();
 	}
 }
