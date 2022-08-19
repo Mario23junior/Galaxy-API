f3b394908311721410a73e9e77b5f0c5ce44c160
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-19 02:14:17 -0300

    Iniciando API de dados sobre sistema de  galaxias

---
+++ src/main/java/com/api/galaxy/GalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.api.galaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/galaxy/GalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.galaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
