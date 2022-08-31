197cec002e50e9007d9207a278c49a900b47fdf5
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-31 02:11:55 -0300

    Criando repository da entidade buracos negros

---
+++ src/main/java/com/api/galaxy/repository/BlackHoleRepositpry.java
@@ -0,0 +1,9 @@
+package com.api.galaxy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.galaxy.model.blackHole;
+
+public interface BlackHoleRepositpry extends JpaRepository<blackHole, Long>{
+	
+}
