7b6d085161a1e6a148adb551cb845d87a6247bae
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-04 08:19:10 -0300

    instanciado service para encapsula logica

--- src/main/java/com/api/galaxy/controller/ConstrollerDayPhotoNasa.java
+++ src/main/java/com/api/galaxy/controller/ConstrollerDayPhotoNasa.java
@@ -1,11 +1,28 @@
 package com.api.galaxy.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.galaxy.dto.DayPhotoNasa;
+import com.api.galaxy.service.ServiceDayPhotoNasa;
+
 @RestController
-@RequestMapping("/project/api/photo")
+@RequestMapping("/project/api/photo/")
 public class ConstrollerDayPhotoNasa {
-   
+    
+	private ServiceDayPhotoNasa service;
+	
+	public ConstrollerDayPhotoNasa(ServiceDayPhotoNasa service) {
+		this.service = service;
+	}
+	
+	public ResponseEntity<DayPhotoNasa> listPhotoDayAniver(){
+		return null;
+		
+	}
+	
+	
 
 }
